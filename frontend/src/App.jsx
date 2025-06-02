db72fe93bd514241d14341ecdc6ff0f1f8901a9e
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-06-02 17:18:04 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Outlet />
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <footer className='border-t'>
+        <div className="container p-4 mx-auto">
+            <p>© All Rights reserved 2025 </p>
+        </div>
+    </footer>
+  )
+}
+
+export default footer
