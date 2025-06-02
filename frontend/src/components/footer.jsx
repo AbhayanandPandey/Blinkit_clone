8e69595078fdd0c7e951e7284c4ab564a5f30bc1
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-06-02 22:03:16 +0530

    updated

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaGithub, FaGoogle, FaLinkedin } from 'react-icons/fa'
 
 function footer() {
   return (
     <footer className='border-t'>
-        <div className="container p-4 mx-auto">
-            <p>© All Rights reserved 2025 </p>
+      <div className="container p-4 mx-auto text-center text-center flex flex-col lg:flex-row lg:justify-between gap-4">
+        <p>© All Rights reserved 2025 </p>
+        <div className='flex item-center gap-4 justify-center text-2xl'>
+          <a href="" className='text-blue-700 hover:-rotate-y-180 transition-transform'>
+            <FaFacebook />
+          </a>
+          <a href="" className='text-shadow-black hover:-rotate-y-180 transition-transform'>
+            <FaGithub />
+          </a>
+          <a href="" className='text-pink-500 hover:-rotate-y-180 transition-transform'>
+            <FaInstagram />
+          </a>
+          <a href="" className='text-blue-600 hover:-rotate-y-180 transition-transform'>
+            <FaLinkedin />
+          </a>
         </div>
+      </div>
+
     </footer>
   )
 }
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function header() {
   return (
-    <header className='h-20 shadow-md'>
-        logo
+    <header className='h-20 shadow-md sticky top-0'>
+      
     </header>
   )
 }
