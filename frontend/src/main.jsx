4d0766d8f642c6a6589e907670fbc5d9b3964f5d
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-06-11 17:48:07 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,8 @@ import router from './routes/index';
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
     <Provider store={store}>
     <RouterProvider router={router} />
 
     </Provider>,
-  // </StrictMode>,
 )
