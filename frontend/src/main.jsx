1be42787490bb413372d31f0839e0a97a2728583
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-06-11 17:48:43 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {RouterProvider} from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import router from './routes/index';
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
 createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
+  <Provider store={store}>
     <RouterProvider router={router} />
-
-    </Provider>,
+  </Provider>,
 )
