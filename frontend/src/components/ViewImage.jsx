72a110e43eebc92397bb6d19b04ddde7abccd6b3
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-06-28 15:37:36 +0530

    updated

--- frontend/src/components/ViewImage.jsx
+++ frontend/src/components/ViewImage.jsx
@@ -6,7 +6,7 @@ const ViewImage = ({url,close}) => {
         <section className="fixed top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center bg-black/50 backdrop-blur-sm p-4">
             <div className="bg-white/0 rounded max-w-md p-5 w-full relative max-h-[80vh] ">
                 <button onClick={close} className=' block w-fit ml-auto cursor-pointer text-white '><IoClose size={25} /></button>
-                <img src={url} alt="full screen "  className='w-full min-h-fit object-scale-down'/>
+                <img src={url} alt="full screen "  className='w-full min-h-fit object-scale-down '/>
             </div>
         </section>
     )
