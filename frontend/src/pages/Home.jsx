394168088498a7cc0534a68c78ae1343e51453b2
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-07-01 22:02:22 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,13 +5,18 @@ import bannerMobile from '../assets/banner-mobile.jpg'
 
 function Home() {
   return (
-    <section>
-      <div className=" w-full mx-auto py-4 px-8">
-        <div className={`w-full h-full max-h-48 bg-blue-100 rounded ${!banner && 'animate-pulse'} `}>
+    <section className='bg-white'>
+      <div className=" w-full mx-auto lg:py-2 lg:px-4">
+        <div className={`w-full h-full md:max-h-52 max-h-96 bg-blue-100 rounded ${!banner && 'animate-pulse'} `}>
           <img
             src={banner}
             alt='banner'
-            className='w-full h-ful '
+            className='w-full h-full hidden md:block '
+          />
+          <img
+            src={bannerMobile}
+            alt='banner'
+            className='w-full h-full md:hidden lg:hidden  '
           />
         </div>
       </div>
