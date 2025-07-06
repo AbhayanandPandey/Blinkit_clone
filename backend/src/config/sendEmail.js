8753514d00b44f1c8d627b99f5d02f7f63658ff2
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-07-06 17:40:49 +0530

    updated

--- backend/src/config/sendEmail.js
+++ backend/src/config/sendEmail.js
@@ -5,6 +5,13 @@ dotenv.config();
 if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
   throw new Error('EMAIL_USER or EMAIL_PASS environment variables are not set');
 }
+const transporter = nodemailer.createTransport({
+  service: 'gmail', 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 const sendEmail = async ({ to, subject, html }) => {
   if (!html) {
