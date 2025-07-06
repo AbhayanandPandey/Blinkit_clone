e08ff23f207bc17990b3adcf72387e4c30385dab
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-07-06 17:42:24 +0530

    updated

--- backend/src/utils/generateAccessToken.js
+++ backend/src/utils/generateAccessToken.js
@@ -5,7 +5,7 @@ const generateAccessToken = async (userId)=>{
     const token = jwt.sign(
         {id:userId},
         process.env.SECRET_KEY_ACCESS,
-        {expiresIn:'1d'}
+        {expiresIn:'7d'}
     )
 
     return token;
