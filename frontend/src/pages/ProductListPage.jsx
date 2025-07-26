63e9dfe66237d248bcf19d37a7a722c9c5cd3178
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-07-26 17:47:03 +0530

    updated

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const ProductListPage = () => {
+  const params = useParams()
+  const 
   return (
-    <div>
-      ProductListPage
-    </div>
+    <section className='bg-white sticky top-24 lg:top-20'>
+      <div className='w-full mx-auto px-4 py-2 grid grid-cols-[90px_1fr] md:grid-cols-[200px_1fr] lg:grid-cols-[280px_1fr]'>
+        <div className='h-full min-h-[76vh]'>
+          <div>
+ffd
+          </div>
+        </div>
+
+        <div className=''>
+fdf
+        </div>
+      </div>
+    </section>
   )
 }
 
