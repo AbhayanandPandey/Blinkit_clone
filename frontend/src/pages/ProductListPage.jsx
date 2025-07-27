3896d8bb062bdde8ee6eeb8fd75514c7b79c3237
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-07-27 17:45:22 +0530

    updated

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -5,7 +5,7 @@ const ProductListPage = () => {
   const params = useParams()
   return (
     <section className='bg-white sticky top-24 lg:top-20'>
-      <div className='w-full mx-auto px-4 py-2 grid grid-cols-[90px_1fr] md:grid-cols-[200px_1fr] lg:grid-cols-[280px_1fr]'>
+      <div className='w-full mx-auto px-4 py-2 grid grid-cols-[90px_1fr]  lg:grid-cols-[280px_1fr] md:grid-cols-[180px_1fr]'>
         <div className='h-full min-h-[76vh]'>
           <div>
 ffd
