839be2408c81ea9b278db815b00df87f36854215
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-07-27 18:03:29 +0530

    updated

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import AxiosToastError from '../utils/AxiosToastError'
+import Axios from '../utils/Axios'
+import Api from '../config/Api'
 
 const ProductListPage = () => {
-  const params = useParams()
+  const [data, setData] = useState([])
+  const [page, setPage] = useState(1)
+  const [loading,setLoading] = useState(false)
+  const [totalPage,setTotalPage] = useState(1)
+
+  const fetchProductData = async ()=>{
+    try {
+      setLoading(true)
+      const response = await Axios({
+        ...Api.getProductsByCategoryAndSubCategory
+      })
+    } catch (error) {
+      AxiosToastError(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <section className='bg-white sticky top-24 lg:top-20'>
       <div className='w-full mx-auto px-4 py-2 grid grid-cols-[90px_1fr]  lg:grid-cols-[280px_1fr] md:grid-cols-[180px_1fr]'>
         <div className='h-full min-h-[76vh]'>
           <div>
-ffd
+            ffd
           </div>
         </div>
 
         <div className=''>
-fdf
+          fdf
         </div>
       </div>
     </section>
