a4578b2ac29cb104e53e33d2fe4ec566bd29b2e6
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-07-30 00:18:40 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
 import Footer from './components/Footer';
 import toast, { Toaster } from 'react-hot-toast';
 import fetchUserDetails from './utils/fetchUserDetails';
