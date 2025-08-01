041ff5ea65ae6d07daf02b91e73d930945418472
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-08-01 19:30:34 +0530

    updated

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -7,6 +7,7 @@ import useMobile from '../hooks/useMobile'
 const Search = () => {
     const navigae = useNavigate()
     const location = useLocation()
+    const [isFocused, setIsFocused] = useState(false);
     const [isSearch, setIsSearch] = useState(false)
     const [isMobile] = useMobile()
     useEffect(() => {
@@ -17,68 +18,87 @@ const Search = () => {
         navigae('/search')
     }
     return (
-        <div className='w-full min-w-[360px] lg:min-[480px] h-11 rounded-lg border border-gray-200 overflow-hidden flex items-center  text-neutral-500 bg-gray-100  group focus-within:border-gray-200 '>
-            <div>
-                
-                {
-                    (isMobile&&isSearch)?(
-                        <Link to={'/'} className='flex justify-center items-center h-full p-2 cursor-pointer group-focus-within:text-yellow-400 bg-white rounded-full shadow-md m-1'>
-                    <FaArrowLeft size={15} />
-                </Link>
-                    ):(
-                        <button className='flex justify-center items-center h-full p-3 cursor-pointer group-focus-within:text-yellow-400'>
-                    <IoSearch size={22} />
-                </button>
-                    )
-                }
-                
-            </div>
-            <div className='w-full h-full'>
-                {
-                    !isSearch ? (
-                        <div onClick={redToSearch} className='w-full h-full flex  items-center'>
-                            <TypeAnimation
-                                sequence={[
-                                    'Search "Milk" ',
-                                    1000,
-                                    'Search "Bread"',
-                                    1000,
-                                    'Search "Icecream"',
-                                    1000,
-                                    'Search "Fruits"',
-                                    1000,
-                                    'Search "Sugar"',
-                                    1000,
-                                    'Search "Paneer"',
-                                    1000,
-                                    'Search "Rice"',
-                                    1000,
-                                    'Search "Egg"',
-                                    1000,
-                                    'Search "Chips"',
-                                    1000,
-                                    'Search "Curd"',
-                                    1000,
-                                ]}
-                                speed={50}
-                                repeat={Infinity}
-                            />
-                        </div>
-                    ) :
-                        (
-                            <div className='w-full h-full'>
-                                <input
-                                    type='text'
-                                    placeholder='Search for Milk Bread and more...'
-                                    autoFocus
-                                    className='bg-transparent w-full h-full outline-none'
+        <>
+            <div className='w-full min-w-[360px] lg:min-w-[480px] h-11 rounded-lg border border-gray-200 overflow-hidden flex items-center justify-between text-neutral-500 bg-gray-100  group focus-within:border-gray-200 '>
+                <div>
+                    {
+                        (isMobile && isSearch) ? (
+                            <Link to={'/'} className='flex justify-center items-center h-[full-5px] w-[full-5px] p-2.5 cursor-pointer group-focus-within:text-gray-400 bg-white rounded-full shadow-md m-1'>
+                                <FaArrowLeft size={15} />
+                            </Link>
+                        ) : (
+                            isFocused ? (
+                                <button
+                                    onMouseDown={() => {
+                                        setIsFocused(false);
+                                        navigae('/'); // <- this ensures you go back to /
+                                    }}
+                                    className="flex justify-center items-center h-full p-3 cursor-pointer group-focus-within:text-gray-400"
+                                >
+                                    <FaArrowLeft size={22} />
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={redToSearch}
+                                    className="flex justify-center items-center h-full p-3 cursor-pointer group-focus-within:text-gray-400"
+                                >
+                                    <IoSearch size={22} />
+                                </button>
+                            )
+
+                        )
+                    }
+                </div>
+
+                <div className='w-full h-full'>
+                    {
+                        !isSearch ? (
+                            <div onClick={redToSearch} className='w-full h-full flex  items-center'>
+                                <TypeAnimation
+                                    sequence={[
+                                        'Search "Milk" ',
+                                        1000,
+                                        'Search "Bread"',
+                                        1000,
+                                        'Search "Icecream"',
+                                        1000,
+                                        'Search "Fruits"',
+                                        1000,
+                                        'Search "Sugar"',
+                                        1000,
+                                        'Search "Paneer"',
+                                        1000,
+                                        'Search "Rice"',
+                                        1000,
+                                        'Search "Egg"',
+                                        1000,
+                                        'Search "Chips"',
+                                        1000,
+                                        'Search "Curd"',
+                                        1000,
+                                    ]}
+                                    speed={50}
+                                    repeat={Infinity}
                                 />
                             </div>
-                        )
-                }
-            </div>
+                        ) :
+                            (
+                                <div className='w-full h-full'>
+                                    <input
+                                        type="text"
+                                        onFocus={() => setIsFocused(true)}
+                                        onBlur={() => setIsFocused(false)}
+                                        placeholder='Search for Milk Bread and more...'
+                                        autoFocus
+                                        className='bg-transparent w-full h-full outline-none'
+                                    />
+                                </div>
+                            )
+                    }
+                </div>
 
-        </div>
+            </div >
+        </>
     )
 }
 
