a0ac018d86715e8770f388f5fcf1d4e8796f71cd
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-08-02 19:05:49 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    host: 'localhost',
+    port: 5173,
+  },
+});
