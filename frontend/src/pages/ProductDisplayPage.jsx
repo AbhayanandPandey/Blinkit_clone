0d9ebbc5c315256c1a8616f092f5c0c76b50cdac
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-08-11 18:54:23 +0530

    updated

--- frontend/src/pages/ProductDisplayPage.jsx
+++ frontend/src/pages/ProductDisplayPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import Api from '../config/Api'
 import Axios from '../utils/Axios'
 import AxiosToastError from '../utils/AxiosToastError'
+import { FaAngleRight, FaAngleLeft } from 'react-icons/fa' 
 
 const ProductDisplayPage = () => {
   const params = useParams()
@@ -41,9 +42,9 @@ const ProductDisplayPage = () => {
 
   console.log(data)
   return (
-    <section className='bg-white mx-auto lg:px-8 md:px-8 p-4 px-3 grid lg:grid-cols-5 '>
+    <section className='bg-white mx-auto lg:px-8 md:px-8 p-4 px-4 grid lg:grid-cols-5 '>
       <div className='col-span-3'>
-        <div className='bg-white lg:min-h-[60vh] lg:max-h-[60vh] rounded min-h-56 max-h-56 h-full w-full'>
+        <div className='bg-white lg:min-h-[60vh] lg:max-h-[60vh] md:min-h-[55vh] md:max-h-[55vh] rounded min-h-60 max-h-60 h-full w-full'>
           <img
             src={data.image[image]}
             alt=""
@@ -54,18 +55,19 @@ const ProductDisplayPage = () => {
           {
             data.image.map((img, i) => {
               return (
-                <div key={i} className={` rounded-full w-5 h-5 ${i === image ? "bg-slate-300 " : "bg-slate-200"} `}>
+                <div key={i} className={` rounded-full w-3 h-3 md:w-5 md:h-5 ${i === image ? "bg-slate-300 " : "bg-slate-200"} `}>
                 </div>
               )
             })
           }
         </div>
-        <div className='grid'>
-          <div className='flex gap-4 lg:items-center lg:justify-center py-3  w-[full+100px] overflow-x-auto scrollbar-hidden mt-3 md:mt-0 p-2 md:p-0 bg-gray-00 lg:bg-white'>
+
+        <div className='grid relative'>
+          <div className='flex gap-4 md:items-center md:justify-center py-3 md:py-3 overflow-x-auto scrollbar-hidden mt-3 md:mt-0 p-2 md:p-0'>
             {
               data.image.map((img, i) => {
                 return (
-                  <div className='h-20 min-h-20 min-w-20 cursor-pointer w-20 shadow  '>
+                  <div className='md:h-35 md:w-35 lg:h-20 lg:w-20 h-20  min-h-20 min-w-20 cursor-pointer w-20 shadow  '>
                     <img
                       src={img}
                       alt='dtat'
@@ -78,6 +80,8 @@ const ProductDisplayPage = () => {
               })
             }
           </div>
+
+          
         </div>
       </div>
       <div>
