d9a8f2fc8fcc4ec6cee2c0e720a72e39480a0845
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-08-16 11:18:28 +0530

    updated

--- frontend/src/pages/ProductDisplayPage.jsx
+++ frontend/src/pages/ProductDisplayPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import Api from '../config/Api'
 import Axios from '../utils/Axios'
 import AxiosToastError from '../utils/AxiosToastError'
+import Divider from '../components/Divider'
 
 const ProductDisplayPage = () => {
   const params = useParams()
@@ -14,6 +15,10 @@ const ProductDisplayPage = () => {
   })
   const [image, setImage] = useState(0)
   const [loading, setLoading] = useState(false)
+
+  const discountedPrice = data.discount
+    ? (data.price - data.price * (data.discount / 100)).toFixed(2)
+    : data.price;
 
   const fetchProductDetails = async () => {
     try {
@@ -41,9 +46,9 @@ const ProductDisplayPage = () => {
 
   console.log(data)
   return (
-    <section className='bg-white mx-auto lg:px-8 md:px-8 p-4 px-4 grid lg:grid-cols-5 '>
-      <div className='col-span-3'>
-        <div className='bg-white lg:min-h-[60vh] lg:max-h-[60vh] md:min-h-[55vh] md:max-h-[55vh] rounded min-h-60 max-h-60 h-full w-full'>
+    <section className='bg-white mx-auto lg:px-8 md:px-8 p-4 px-4 grid lg:grid-cols-2 '>
+      <div className='col-span-1'>
+        <div className='bg-white lg:min-h-[60vh] lg:max-h-[60vh] md:min-h-[55vh] md:max-h-[55vh] rounded min-h-65 max-h-70 h-full w-full'>
           <img
             src={data.image[image]}
             alt=""
@@ -62,7 +67,7 @@ const ProductDisplayPage = () => {
         </div>
 
         <div className='grid relative'>
-          <div className='flex gap-4 md:items-center md:justify-center py-3 md:py-3 overflow-x-auto scrollbar-hidden mt-3 md:mt-0 p-2 md:p-0'>
+          <div className='flex gap-4 md:items-center md:justify-center py-3 md:py-3 overflow-x-auto scrollbar-hidden md:mt-0 p-2 md:p-0'>
             {
               data.image.map((img, i) => {
                 return (
@@ -71,8 +76,8 @@ const ProductDisplayPage = () => {
                       src={img}
                       alt='dtat'
                       key={i}
-                      onClick={()=>setImage(i)}
-                      className={`w-full h-full object-scale-down  ${i===image ? "border-2 border-gray-300" : "border border-gray-200"} `}
+                      onClick={() => setImage(i)}
+                      className={`w-full h-full object-scale-down  ${i === image ? "border-2 border-gray-300" : "border border-gray-200"} `}
                     />
                   </div>
                 )
@@ -82,8 +87,18 @@ const ProductDisplayPage = () => {
         </div>
       </div>
 
-      <div>
-
+      <div className='col-span-1 p-4 pl-0 pt-3 lg:pt-4 lg:pl-6 text-base md:text-lg'>
+        <p className='text-sm text-black rounded-full w-fit bg-green-300 mt-2 p-2 py-[0.5px] m-2 ml-0'>10 Min</p>
+        <h2 className='text-lg font-semibold md:text-xl lg:text-2xl'>{data.name}</h2>
+        <p>{data.unit}</p>
+        <Divider />
+        <p>Price</p>
+        <div className="border-2 bg-green-50 w-fit border-green-600 px-4 py-2 rounded flex items-center gap-2 ">
+          <span className="text-green-600 text-lg md:text-xl font-semibold ">₹{Math.round(discountedPrice)}</span>
+          {data.discount > 0 && (
+            <span className="text-sm line-through text-gray-500 ">₹{Math.round(data.price)}</span>
+          )}
+        </div>
       </div>
     </section>
   )
