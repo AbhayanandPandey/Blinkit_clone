b35019b7b8fb6f3494bbb44aa1a6ef9059d05cbe
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-08-21 18:06:37 +0530

    updated

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -4,23 +4,22 @@ import SearchSkeleton from '../Skeleton/SearchSkeleton'
 const SearchPage = () => {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
-  const loadingCard = new Array(10).fill(null)
+  const loadingCard = new Array(12).fill(null)
   return (
     <section className='bg-white lg:min-h-[79vh]  md:min-h-[81vh] min-h-[78vh]'>
       <div className=' mx-auto p-4 md:px-6'>
         <p className='font-semibold'>Search Results: {data.length}</p>
         <div className='py-4 grid grid-cols-2 place-items-center sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 grid-c gap-5'>
           {
-            !loading ? (
+            !loading && (
               loadingCard.map((_, i) => {
                 return (
                   <SearchSkeleton key={i} />
                 )
               })
-            ) : (
-              ''
             )
           }
+
         </div>
       </div>
     </section>
