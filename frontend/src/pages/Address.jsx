bb277eaed64b41230a6a2f6b54aa7968ae862464
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-09-28 08:20:41 +0530

    updated

--- frontend/src/pages/Address.jsx
+++ frontend/src/pages/Address.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useGlobal } from "../provider/GlobalProvider";
 import AddAddress from "./AddAddress";
 import NoData from "../components/NoData";
-import { MdOutlineEdit, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineEdit, MdOutlineDelete, MdDelete, MdEdit } from "react-icons/md";
 import ConfirmDelete from "../components/ConfirmDelete";
 import toast from "react-hot-toast";
 
@@ -67,12 +67,24 @@ const Address = () => {
             {addressList && addressList.length > 0 ?
               (
                 addressList.map((a) => (
-                  <div key={a._id} className="shadow-md rounded-xl bg-white px-5 py-4 border border-gray-200 hover:border-blue-100 hover:shadow-lg transition-all cursor-pointer" >
-                    <p className="font-semibold text-gray-800">
-                      {a.address_line}
-                    </p>
-                    <p className="text-gray-600"> {a.city}, {a.state} </p> <p className="text-gray-600"> {a.country} - {a.pincode} </p>
-                    <p className="text-sm text-gray-500">Mobile: {a.mobile}</p>
+                  <div key={a._id} className="shadow-md rounded-xl bg-white px-5 py-4 border border-gray-200 hover:border-blue-100 hover:shadow-lg transition-all flex gap-2 justify-between" >
+
+                    <div>
+                      <p className="font-semibold text-gray-800">
+                        {a.address_line}
+                      </p>
+                      <p className="text-gray-600"> {a.city}, {a.state} </p> <p className="text-gray-600"> {a.country} - {a.pincode} </p>
+                      <p className="text-sm text-gray-500">Mobile: {a.mobile}</p>
+                    </div>
+                    <div className="flex flex-col justify-around">
+                      <span className="cursor-pointer text-red-500">
+                        <MdDelete size={25} />
+                      </span>
+
+                      <span className="cursor-pointer text-green-600">
+                        <MdEdit size={25} />
+                      </span>
+                    </div>
                   </div>))) :
               (<p className="text-gray-500">No addresses found.</p>)} </div> </div> </div>
 
