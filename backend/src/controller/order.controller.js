44d015222ff911efa0e4a403780a580ac2cad0ff
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-09-30 08:54:28 +0530

    updated

--- backend/src/controller/order.controller.js
+++ backend/src/controller/order.controller.js
@@ -5,8 +5,9 @@ export function CashPayment(req, res) {
     try {
         const userId = req.userId
         const {list_items, totalAmt, addressId, subTotalAmt,  } = req.body
-
         
+
+        console.log("f",list_items, "f",totalAmt, "f",addressId, "f",subTotalAmt,)
     } catch (error) {
         return res.status(500).json({
             error: true,
